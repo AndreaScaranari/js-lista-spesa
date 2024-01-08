6a9ecc3564a91dea58059c7306b46560ebab0180
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-01-08 16:07:13 +0100

    ciclo_while

--- js/script.js
+++ js/script.js
@@ -5,19 +5,40 @@ const ulGroceryList = document.querySelector("ul");
 const groceryList = ["pane", "latte", "pasta", "uova", "carne", "pesce", "verdura", "frutta", "gelati", "snacks"];
 console.log(groceryList);
 
-// ciclo For con manipolazione del DOM
+// * ciclo For con manipolazione del DOM
 // for (let i = 0; i < groceryList.length; i++) {
 //     const liGroceryList = document.createElement("li");
 //     liGroceryList.append(groceryList[i]);
 //     ulGroceryList.appendChild(liGroceryList);
 // }
 
-// ciclo For con template literal
-let liGroceryList = "";
+// * ciclo For con template literal
+// let liGroceryList = "";
 
-for (let i = 0; i < groceryList.length; i++) {
-    liGroceryList += `<li>${(groceryList[i])}</li>`;
+// for (let i = 0; i < groceryList.length; i++) {
+//     liGroceryList += `<li>${(groceryList[i])}</li>`;
+// }
+
+// ulGroceryList.innerHTML = liGroceryList;
+
+// * ciclo While con manipolazione del DOM
+let i = 0;
+
+while (i < groceryList.length - 1) {
+    const liGroceryList = document.createElement("li");
+    liGroceryList.append(groceryList[i]);
+    ulGroceryList.appendChild(liGroceryList);
+
+    i++;
 }
 
-ulGroceryList.innerHTML = liGroceryList;
+// * ciclo While con template literal
+// let liGroceryList = "";
 
+// while (i < groceryList.length - 1) {
+//     liGroceryList += `<li>${(groceryList[i])}</li>`;
+
+//     i++;
+// }
+
+// ulGroceryList.innerHTML = liGroceryList;
